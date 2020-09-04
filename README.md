b5059488122ed7abb4c2ee7cb8ae67bafc718d63
Vilhelm Prytz <vilhelm@prytznet.se>
2020-09-04 14:52:48 +0200

    Initial application

---
+++ README.md
@@ -0,0 +1,3 @@
+# voltage-measurement
+
+Measure voltage using Arduino. Green LED is powered on if voltage is above 1.5 V. Red LED is powered on if voltage is below that.
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,41 @@
 #include <Arduino.h>
 
+int analogPin = A0; // analog pin 0
+int val = 0; // current analog value
+float voltage = 0; // current calculated voltage
+
+int green_led = 12; // LED that is connected to green
+int red_led = 13; // LED that is connected to red
+
 void setup() {
-  // put your setup code here, to run once:
+  // ability to print back value to connected computer
+  Serial.begin(9600);
+
+  // pin 12 = green lamp
+  pinMode(green_led, OUTPUT);
+
+  // pin 13 = red lamp
+  pinMode(red_led, OUTPUT);
 }
 
+// 1023 --> 5 V
+// 657 --> 3.3 V
+// 0 --> 0 V
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  val = analogRead(analogPin); // read the current value
+  voltage = 5.0 * val / 1024; // since the analog input only has 1024 bits this is how we convert to volt
+  Serial.println(voltage); // print value back to us
+
+  // updating continuously floods the connection
+  delay(500); // update very 500
+
+  // if voltage above 1.5, all good
+  if (voltage > 1.5) {
+    digitalWrite(green_led, HIGH); // power on green
+    digitalWrite(red_led, LOW); // power off red
+  } else { // not OK!
+    digitalWrite(green_led, LOW); // power off green
+    digitalWrite(red_led, HIGH); // power on red
+  }
 }
